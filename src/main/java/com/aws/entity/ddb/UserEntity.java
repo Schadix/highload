946cdaece6453b77c09bc382e563f931d537a0d9
Martin Schade <schadix@gmail.com>
2014-03-11 15:26:15 -0400

    init

---
+++ src/main/java/com/aws/entity/ddb/UserEntity.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.aws.entity.ddb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author schadem
+ */
+@DynamoDBTable(tableName = "Users")
+@XmlRootElement
+public class UserEntity {
+
+    private String account;
+    private String bets;
+    private String insertDate;
+
+    @DynamoDBHashKey
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    @DynamoDBAttribute
+    public String getInsertDate() {
+        return insertDate;
+    }
+
+    public void setInsertDate(String insertDate) {
+        this.insertDate = insertDate;
+    }
+    
+    @DynamoDBAttribute
+    public String getBets() {
+        return bets;
+    }
+
+    public void setBets(String bets) {
+        this.bets = bets;
+    }
+}
